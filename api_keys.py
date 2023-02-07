ed6556d9eb8003de759ebc13707d509551893dce
diego-lazaro <115186079+diego-lazaro@users.noreply.github.com>
2023-02-06 18:38:11 -0800

    updated code

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "fabfc272e04e0ded4dff220d1f4228c1"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "528ad42f1e2841daa16c7ea5034a5b5f"
+
